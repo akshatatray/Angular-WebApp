c580da0b37f6b5a0705ceab13943e0db13ac2331
AKSHAT <akthd4@gmail.com>
2020-05-28 17:39:05 +0530

    Bug Fix and added Logo

--- src/app/components/theme-changer/theme-changer.component.ts
+++ src/app/components/theme-changer/theme-changer.component.ts
@@ -15,10 +15,18 @@ export class ThemeChangerComponent implements OnInit {
   theme() {
     const body = document.getElementsByTagName('body')[0];
     const dl = body.className;
+
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+      body.classList.add(theme);
+    }
+
     if (dl == 'dark') {
       body.classList.replace('dark' , 'light');
+      localStorage.setItem('theme','light')
     } else if (dl == 'light') {
       body.classList.replace('light' , 'dark');
+      localStorage.setItem('theme','dark')  
     }
   }
 
